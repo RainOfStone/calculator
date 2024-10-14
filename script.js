b810ca80da652bec39c2db44c887b8d84c2cc141
RainOfStone <RainOfStone>
2024-10-13 23:24:05 -0400

    created functions and calculator works for single digits

--- script.js
+++ script.js
@@ -16,59 +16,208 @@ multiply = document.querySelector('#multiply'),
 divide = document.querySelector('#divide'),
 AC = document.querySelector('#AC'),
 equal  = document.querySelector('#equal')
-let num1, num1Plus, num2, op, // operator
+let num1, num1Plus, num2 = 'none', op = 'none', // operator
 sign, // positive or negative
-specialbtn //ac or equal
+specialbtn, //ac or equal
+Val1, Val2, //represent the first and second num
+FuncRan, op2 
 nums.addEventListener('click', (event) => {
     let target = event.target
     switch(target.id) {
         case 'zero':
-            num1 = '0'
+            Val1 = 1
+            if (op == 'none') {
+                num1 = '0'
+                DisplayNum1()
+            }
+            else if (op != 'none'){
+                num2 = '0'
+                DisplayNum2()
+            }
             break  
         case 'one':
-            num1 = '1'
+            Val1 = 1
+            if (op == 'none') {
+                num1 = '1'
+                DisplayNum1()
+            }
+            else if (op != 'none'){
+                num2 = '1'
+                DisplayNum2()
+            }
             break
         case 'two':
-            num1 = '2'
+            Val1 = 1
+            if (op == 'none') {
+                num1 = '2'
+                DisplayNum1()
+            }
+            else if (op != 'none'){
+                num2 = '2'
+                DisplayNum2()
+            }
             break
         case 'three':
-            num1 = '3'
+            Val1 = 1
+            if (op == 'none') {
+                num1 = '3'
+                DisplayNum1()
+            }
+            else if (op != 'none'){
+                num2 = '3'
+                DisplayNum2()
+            }
             break
         case 'four':
-            num1 = '4'
+            Val1 = 1
+            if (op == 'none') {
+                num1 = '4'
+                DisplayNum1()
+            }
+            else if (op != 'none'){
+                num2 = '4'
+                DisplayNum2()
+            }
             break
         case 'five':
-            num1 = '5'
+            Val1 = 1
+            if (op == 'none') {
+                num1 = '5'
+                DisplayNum1()
+            }
+            else if (op != 'none'){
+                num2 = '5'
+                DisplayNum2()
+            }
             break 
         case 'six':
-            num1 = '6'
+            Val1 = 1
+            if (op == 'none') {
+                num1 = '6'
+                DisplayNum1()
+            }
+            else if (op != 'none'){
+                num2 = '6'
+                DisplayNum2()
+            }
             break     
         case 'seven':
-            num1 = '7'
+            Val1 = 1
+            if (op == 'none') {
+                num1 = '7'
+                DisplayNum1()
+            }
+            else if (op != 'none'){
+                num2 = '7'
+                DisplayNum2()
+            }
             break   
         case 'eight':
-            num1 = '8'
+            Val1 = 1
+            if (op == 'none') {
+                 num1 = '8'
+                DisplayNum1()
+            }
+            else if (op != 'none'){
+                num2 = '8'
+                DisplayNum2()
+            }
             break
         case 'nine':
-            num1 = '9'
+            Val1 = 1
+            if (op == 'none') {
+                num1 = '9'
+                DisplayNum1()
+            }
+            else if (op != 'none'){
+                num2 = '9'
+                DisplayNum2()
+            }
             break
         case 'add':
-            op = 'add'
+            if (op == 'none') {
+                op = '+'
+                DisplayOp1()
+            }
+            else if (op != 'none') {
+                op2 = '+'
+                DisplayOp2()
+            }
             break
         case 'subtract':
-            op = 'subtract'
+            if (op == 'none') {
+                op = '-'
+                DisplayOp1()
+            }
+            else if (op != 'none') {
+                op2 = '-'
+                DisplayOp2()
+            }
             break
         case 'multiply':
-            op = 'multiply'
+            if (op == 'none') {
+                op = '*'
+                DisplayOp1()
+            }
+            else if (op != 'none') {
+                op2 = '*'
+                DisplayOp2()
+            }
             break
         case 'divide':
-            op = 'divide'
+            if (op == 'none') {
+                op = '/'
+                DisplayOp1()
+            }
+            else if (op != 'none') {
+                op2 = '/'
+                DisplayOp2()
+            }
             break
         case 'AC':
             specialbtn = 'AC'
+            Clear()
             break
         case 'equal':  
             specialbtn = 'equal'
             break       
     }
 })
+function DisplayNum1() {
+    if (Val1 == 1 && op == 'none') {
+        answers.textContent = `${num1}` 
+        FuncRan = 'FuncNum1'
+    }
+}
+function DisplayNum2() {
+    answers.textContent = `${num1} ${op} ${num2}`
+}
+function DisplayOp1() {
+    if (Val1 == 1 && op != 'none') {
+        answers.textContent = `${num1} ${op}`
+        Val1 = 0
+        FuncRan = 'FuncOp1'
+    }
+}
+function DisplayOp2() {
+    if (op == '+') {
+        num1 = Number(num1) + Number(num2)
+        console.log(num1)
+        answers.textContent = `${num1} ${op2}`
+    }
+    else if (op == '-') {
+        num1 = Number(num1) - Number(num2)
+        answers.textContent = `${num1} ${op2}` 
+    }
+    else if (op == '*') {
+        num1 = Number(num1) * Number(num2)
+        answers.textContent = `${num1} ${op2}` 
+    }
+    else if (op == '/') {
+        num1 = Number(num1) / Number(num2)
+        answers.textContent = `${num1} ${op2}` 
+    }
+}
+function Clear() {
+    answers.textContent = ''
+}
